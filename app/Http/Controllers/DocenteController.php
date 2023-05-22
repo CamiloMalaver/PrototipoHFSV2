e0ff95122a7def46e91ae596c745611c5d07f8af
CamiloMalaver <62223822+CamiloMalaver@users.noreply.github.com>
2023-05-22 09:33:24 -0500

    another attemp mimes

--- app/Http/Controllers/DocenteController.php
+++ app/Http/Controllers/DocenteController.php
@@ -56,8 +56,7 @@ class DocenteController extends Controller
             'descripcion_actividad' => 'required|string|max:400|min:20',
             'observaciones' => 'required|string|max:400|min:20',
             'evidencias' => 'required|array',
-            'evidencias.*' => 'required|mimetypes:txt/pdf/doc/docx/jpg/jpeg',
-            'evidencias' => 'max:2048',
+            'evidencias.*' => 'mimes:pdf,txt,doc,docx,jpg,jpeg|max:2048',
         ]);
 
         $funcion = FuncionSustantiva::find($request->funcion_id);
